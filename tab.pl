f570b5e5c7024f0df8d7d7031a1ebcf74ba083af
Hao Wu <hwu@intellisurvey.com>
2015-04-11 14:10:38 -0700

    init

--- tab.pl
+++ tab.pl
@@ -1,19 +1,24 @@
 use v5.10;
+use strict;
+use warnings;
 
 my @aminos =
-  qw(ASP GLU ASN SER GLN HIS GLY THR CIT ARG b-ALA   TAU ALA TYR TRP MET VAL PHE ILE LEU ORN LYS);
+  qw(ASP GLU ASN SER GLN HIS GLY THR CIT ARG b-ALA TAU ALA TYR TRP MET VAL PHE ILE LEU ORN LYS);
+
+# the header
 say( join ',', '""', @aminos );
+
 opendir( my $dh, '.' ) || die;
+
 while ( readdir $dh ) {
-    my ( $sample, $data_href ) = get_data_from_file($_) if /\.ars/;
-    if ($sample) {
-        say( join ',', $sample,
-            map { $data_href->{ '"' . $_ . '"' } } @aminos );
+    my ( $name, $data ) = get_data_from_file($_) if /\.ars/;
+    if ($name) {
+        # the data rows
+        say( join ',', $name,
+            map { $data->{ '"' . $_ . '"' } } @aminos );
     }
 }
 closedir $dh;
-
-### sub ###
 
 sub read_file_to_array {
     my $file     = shift;
@@ -23,15 +28,23 @@ sub read_file_to_array {
           or die "could not open $file: $!";
         <$fh>;
     };
+    # annoying line feed between windows and linux
     $document =~ s/(\012|\015\012?)/\012/g;
     my @lines = split '\n', $document;
     return @lines;
 }
 
-sub get_required_lines {
+sub get_sample_name {
+    my ($sample_line) = grep { /Sample Name/ } @_;
+    my $sample_name = ( split '\t', $sample_line )[1];
+    return $sample_name;
+}
+
+sub get_sample_data {
     my @all_lines = @_;
     my $result;
     my @required_lines;
+    # use a boolean to indicate the start and end
     my $process_line = 0;
     for my $line (@all_lines) {
         $process_line = 1 if $line =~ /"Amount \(nmol\/ml\)"/;
@@ -44,17 +57,12 @@ sub get_required_lines {
     return $result;
 }
 
-sub get_sample_name {
-    my ($sample_line) = grep { /Sample Name/ } @_;
-    my $sample_name = ( split '\t', $sample_line )[1];
-    return $sample_name;
-}
 
 sub get_data_from_file {
     my $file_name = shift;
     my @lines     = read_file_to_array($file_name);
-    my $sample    = get_sample_name(@lines);
-    my $data_href = get_required_lines(@lines);
-    return ( $sample, $data_href );
+    my $name    = get_sample_name(@lines);
+    my $data = get_sample_data(@lines);
+    return ( $name, $data );
 }
 
